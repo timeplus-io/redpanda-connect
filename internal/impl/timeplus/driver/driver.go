aa5337b39d420d56d0d4cca10b7b22f975de95c6
Calvin Zhang <calvin.zhang@timeplus.io>
2024-09-27 15:49:04 -0700

    feat: added timeplus input

---
+++ internal/impl/timeplus/driver/driver.go
@@ -0,0 +1,136 @@
+package driver
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"io"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+	driver "github.com/timeplus-io/proton-go-driver/v2"
+)
+
+type DriverImpl struct {
+	logger      *service.Logger
+	conn        *sql.DB
+	rows        *sql.Rows
+	columnTypes []*sql.ColumnType
+}
+
+var (
+	codeRe = *regexp.MustCompile(`code: (.+[0-9])`)
+	msgRe  = *regexp.MustCompile(`message: (.*)`)
+)
+
+func NewDriver(logger *service.Logger, addr, username, password string) *DriverImpl {
+	conn := driver.OpenDB(&driver.Options{
+		Addr: []string{addr},
+		Auth: driver.Auth{
+			Username: username,
+			Password: password,
+		},
+		DialTimeout: 5 * time.Second,
+	})
+
+	return &DriverImpl{
+		logger: logger,
+		conn:   conn,
+	}
+}
+
+func (d *DriverImpl) Run(ctx context.Context, sql string) error {
+	ckCtx := driver.Context(ctx, driver.WithProgress(func(p *driver.Progress) {
+		// no-op
+	}))
+	rows, err := d.conn.QueryContext(ckCtx, sql)
+	if err != nil {
+		return err
+	}
+
+	d.rows = rows
+
+	columnTypes, err := rows.ColumnTypes()
+	if err != nil {
+		return err
+	}
+
+	d.columnTypes = columnTypes
+
+	return nil
+}
+
+func (d *DriverImpl) Read(ctx context.Context) (map[string]any, error) {
+	for { // retry loop
+		if d.rows.Next() {
+			count := len(d.columnTypes)
+
+			values := make([]any, count)
+			valuePtrs := make([]any, count)
+
+			for i := range d.columnTypes {
+				valuePtrs[i] = &values[i]
+			}
+
+			if err := d.rows.Scan(valuePtrs...); err != nil {
+				return nil, err
+			}
+
+			event := make(map[string]any)
+			for i, col := range d.columnTypes {
+				event[col.Name()] = values[i]
+			}
+
+			return event, nil
+		}
+
+		if err := d.rows.Err(); err != nil {
+			if errors.Is(err, context.Canceled) || IsQueryCancelErr(err) {
+				// Most likely timeplusd got restarted. Since we are going to re-connect to timeplusd once it recovered, we do not log it as error for now.
+				d.logger.With("reason", err).Info("query cancelled")
+				return nil, io.EOF
+			}
+
+			d.logger.With("error", err).Errorf("query failed: %s", err.Error())
+			// this happens when the SQL is updated, i.e. a new MV is created, the previous checkpoint is on longer available.
+			if strings.Contains(err.Error(), "code: 2003") {
+				continue // retry
+			}
+			return nil, err
+		}
+		return nil, io.EOF
+	}
+}
+
+func (d *DriverImpl) Close() error {
+	if d.rows != nil {
+		d.rows.Close()
+	}
+	return d.conn.Close()
+}
+
+func IsQueryCancelErr(err error) bool {
+	code, msg := Parse(err)
+	return code == 394 && strings.Contains(msg, "Query was cancelled")
+}
+
+func Parse(err error) (int, string) {
+	var code int
+	var msg string
+
+	errStr := err.Error()
+	codeMatches := codeRe.FindStringSubmatch(errStr)
+	if len(codeMatches) == 2 {
+		code, _ = strconv.Atoi(codeMatches[1])
+	}
+
+	msgMatches := msgRe.FindStringSubmatch(errStr)
+	if len(msgMatches) == 2 {
+		msg = msgMatches[1]
+	}
+
+	return code, msg
+}
---
+++ internal/impl/timeplus/input.go
@@ -0,0 +1,130 @@
+package timeplus
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"syscall"
+
+	"github.com/redpanda-data/benthos/v4/public/service"
+	"github.com/redpanda-data/connect/v4/internal/impl/timeplus/driver"
+	"github.com/redpanda-data/connect/v4/internal/impl/timeplus/http"
+)
+
+var inputConfigSpec *service.ConfigSpec
+
+func init() {
+	inputConfigSpec = service.NewConfigSpec().
+		Summary("Reads messages from Timeplus Enterprise.").
+		Field(service.NewStringField("query")).
+		Field(service.NewURLField("url").Default("tcp://localhost:8463")).
+		Field(service.NewStringField("username")).
+		Field(service.NewStringField("password").Secret())
+
+	err := service.RegisterInput(
+		"timeplus", inputConfigSpec, newTimeplusInput)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func newTimeplusInput(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+	logger := mgr.Logger()
+	sql, err := conf.FieldString("query")
+	if err != nil {
+		return nil, err
+	}
+
+	addr, err := conf.FieldURL("url")
+	if err != nil {
+		return nil, err
+	}
+
+	username, err := conf.FieldString("username")
+	if err != nil {
+		return nil, err
+	}
+
+	password, err := conf.FieldString("password")
+	if err != nil {
+		return nil, err
+	}
+
+	var reader Reader
+
+	if addr.Scheme == "tcp" {
+		reader = driver.NewDriver(logger, addr.Host, username, password)
+	} else {
+		reader = http.NewSSEClient(logger, addr, "test", "test", "hello", username, password)
+	}
+
+	return service.AutoRetryNacks(
+		&timeplusInput{
+			log:    logger,
+			reader: reader,
+			sql:    sql,
+		}), nil
+}
+
+type timeplusInput struct {
+	log *service.Logger
+
+	reader Reader
+	sql    string
+
+	stopQuery context.CancelFunc
+}
+
+func (p *timeplusInput) Connect(ctx context.Context) error {
+	logger := p.log.With("sql", p.sql)
+	queryCtx, stopQuery := context.WithCancel(context.Background())
+	p.stopQuery = stopQuery
+
+	if err := p.reader.Run(queryCtx, p.sql); err != nil {
+		if errors.Is(err, syscall.ECONNREFUSED) {
+			return fmt.Errorf("failed to connect to driver")
+		} else if errors.Is(err, os.ErrDeadlineExceeded) {
+			return fmt.Errorf("failed to connect to driver")
+		}
+
+		return fmt.Errorf("failed to run query: %w", err)
+	}
+
+	logger.Info("timeplusd connected")
+
+	return nil
+}
+
+func (p *timeplusInput) Read(ctx context.Context) (*service.Message, service.AckFunc, error) {
+	event, err := p.reader.Read(ctx)
+	if err != nil {
+		// timeplusd input always read from a MV which is supposed will never end,
+		// Thus, if it encounters an EOF error, that means the connection is terminated for some reason, for example,
+		// during timeplusd upgrade, the pod is terminated for a new one. So here, we return ErrNotConnected so that
+		// bethos will retry reconnect to timeplusd.
+		if errors.Is(err, io.EOF) {
+			return nil, nil, service.ErrNotConnected
+		}
+
+		// TODO check if it's possible to identify if it's a connection error or not, if it's a connection error,
+		//      it should return `service.ErrNotConnected` so that benthos will handle reconnection
+		return nil, nil, err
+	}
+
+	msg := service.NewMessage(nil)
+	msg.SetStructured(event)
+
+	ack := func(ctx context.Context, err error) error {
+		// Nacks are retried automatically when we use service.AutoRetryNacks
+		return nil
+	}
+
+	return msg, ack, nil
+}
+
+func (p *timeplusInput) Close(ctx context.Context) error {
+	p.stopQuery()
+	return p.reader.Close()
+}
